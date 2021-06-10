01540caa217924207d931de06da53a5779b1425f
horverno <horverno@gmail.com>
2021-06-10 09:28:37 +0200

    modify node for 3d lidar

--- script/circle_fitting.py
+++ script/circle_fitting.py
@@ -5,72 +5,166 @@ import numpy as np
 import visualization_msgs.msg as vismsg
 import ros_numpy
 import sensor_msgs.msg as senmsg
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon,Point
 import autoware_msgs.msg as autoware
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+import geometry_msgs.msg as geomsg
 
-free_space=None
+P= None
+free_space = None
+valid_circles = None
+slope = None
 
 
 def point_cloud_callback(data):
-    global free_space
+    global free_space, valid_circles
     pc = ros_numpy.numpify(data)
     points=np.zeros((pc.shape[0],3))
     points[:,0]=pc['x']
     points[:,1]=pc['y']
     points[:,2]=pc['z']
 
-    free_space=Polygon([[np.min(points[:,0]),np.max(points[:,1])],[np.max(points[:,0]),np.max(points[:,1])],[np.max(points[:,0]),np.max(points[:,1])],[np.min(points[:,0]),np.min(points[:,1])]])
 
+    zmask = points[:,2] > -0.4
+    points_zmasked = points[zmask] 
+    clustering=DBSCAN(eps=0.75,min_samples=10).fit(points_zmasked[:,0:2])
 
+    # plt.scatter(points_zmasked[:,0],points_zmasked[:,1])
+    # plt.show()
+    
+    number_clusters= len(set(clustering.labels_)) - (1 if -1 in clustering.labels_ else 0)
+    b=np.zeros((number_clusters,3))
+    points_zmasked=np.column_stack((points_zmasked,clustering.labels_))
+    for i in range(number_clusters):
+        a=np.where(points_zmasked[:,3]==i)
 
-def callback_detectedobjects(data):
+        xc,yc,r=fit_circle_2d(points_zmasked[a][:,0],points_zmasked[a][:,1])
+        b[i,0]=xc
+        b[i,1]=yc
+        b[i,2]=r
+    
+
+    circles_index = np.intersect1d(np.where(b[:,2] < 2.0),np.where(b[:,2] > 0.1))
+    valid_circles = b[circles_index]
+
+    #free_space=Polygon([[np.min(points[:,0]),np.max(points[:,1])],[np.max(points[:,0]),np.max(points[:,1])],[np.max(points[:,0]),np.max(points[:,1])],[np.min(points[:,0]),np.min(points[:,1])]])
+
+def callback_markerarray(data):
+    global P,slope
     polygon_list=[]
-    polygon=[]
+    polygons=[]
+    markers_list=[]
+    c=[]
     
+    for i in range(len(data.markers)):
+        markers=[]
+        #print(i)
+
+        #markers=np.empty([0])
+        
+        # for j in range(len(data.markers[i].points)):
+        #     np.append(markers,[data.markers[i].points[j].x,data.markers[i].points[j].y])
+
+        for j in range(len(data.markers[i].points)):
+            markers.append([data.markers[i].points[j].x,data.markers[i].points[j].y]) 
+        markers_list.append(markers)
+    free_space=Polygon(np.concatenate(markers_list)).minimum_rotated_rectangle
+    #free_space=(np.concatenate(markers_list))
+
+    if free_space.exterior.coords.xy[0][0] > free_space.exterior.coords.xy[0][1]: 
+        slope=np.arctan2((free_space.exterior.coords.xy[1][0]-free_space.exterior.coords.xy[1][1]),(free_space.exterior.coords.xy[0][0]-free_space.exterior.coords.xy[0][1]))
+    elif free_space.exterior.coords.xy[0][0] < free_space.exterior.coords.xy[0][1]: 
+        slope=np.arctan2((free_space.exterior.coords.xy[1][1]-free_space.exterior.coords.xy[1][0]),(free_space.exterior.coords.xy[0][1]-free_space.exterior.coords.xy[0][0])) 
+
+    #     polygon_list.append(markers)
+    # for k in range(len(polygon_list)):        
+    #     polygons.append(Polygon(polygon_list[k]))
     
-    for i in range (len(data.objects)):
-        polygon_data=np.zeros((len(data.objects[i].convex_hull.polygon.points),2))
-        for j in range(len(data.objects[i].convex_hull.polygon.points)):
-            polygon_data[j,0]=data.objects[i].convex_hull.polygon.points[j].x
-            polygon_data[j,1]=data.objects[i].convex_hull.polygon.points[j].y
-        polygon_list.append(polygon_data)
-    polygon_list=np.array(polygon_list)  
-
-    a=np.zeros((len(polygon_list),3))
-    
-    for i in range(len(a)):
-        xc,yc,r=fit_circle_2d(polygon_list[i][:,0],polygon_list[i][:,1])
-        a[i,0]=xc
-        a[i,1]=yc
-        a[i,2]=r
-
-
-    
-    circles=np.intersect1d(np.where(a[:,2] < 3.0),np.where(a[:,2] > 0.2))  
-
-    for i in circles:
-        p=Polygon(polygon_list[i])
-        if free_space.intersects(p):
-            polygon.append(i)
-
-    valid_polygon=a[polygon]
-
-    print(len(valid_polygon),valid_polygon)
-    # print(valid_polygon)
 
     
-    # for k in range(len(polygon_list)):
-    #     p=Polygon(polygon_list[k])
-    #     polygon.append(p)
+    if valid_circles is not None:
+        for i in range(len(valid_circles)):
+            c.append((free_space).contains(Point(valid_circles[i,0],valid_circles[i,1]).buffer(0.2)))
+                #c.append(i)
+
+        valid_circles_=valid_circles[c]
+
+        if len(valid_circles_)==2:
+            P=((valid_circles_[0,0] + valid_circles_[1,0])/2,(valid_circles_[0,1] + valid_circles_[1,1])/2)
+        #     pass
+    
+    
+    # plt.plot(free_space.exterior.coords.xy[0],free_space.exterior.coords.xy[1])
+    # plt.scatter(valid_circles[:,0],valid_circles[:,1])
+    # plt.show()
+
+    # points=data.points 
+    
+    # a=np.zeros((5,2))
+
+    # polygon_data= np.zeros((len(points),2)) 
+    # for i in range (len(points)):
+    #     polygon_data[i,0]=points[i].x
+    #     polygon_data[i,1]=points[i].y
+    
+    # free_space=Polygon(polygon_data).minimum_rotated_rectangle
+
+    # print('a')
+    # # if valid_circles is not None:
+    # #     for i in range(len(valid_circles)):
+
+
+
+
+# def callback_detectedobjects(data):
+#     polygon_list=[]
+#     polygon=[]
+    
+    
+#     for i in range (len(data.objects)):
+#         polygon_data=np.zeros((len(data.objects[i].convex_hull.polygon.points),2))
+#         for j in range(len(data.objects[i].convex_hull.polygon.points)):
+#             polygon_data[j,0]=data.objects[i].convex_hull.polygon.points[j].x
+#             polygon_data[j,1]=data.objects[i].convex_hull.polygon.points[j].y
+#         polygon_list.append(polygon_data)
+#     polygon_list=np.array(polygon_list)  
+
+#     a=np.zeros((len(polygon_list),3))
+    
+#     for i in range(len(a)):
+#         xc,yc,r=fit_circle_2d(polygon_list[i][:,0],polygon_list[i][:,1])
+#         a[i,0]=xc
+#         a[i,1]=yc
+#         a[i,2]=r
+
+
+    
+#     circles=np.intersect1d(np.where(a[:,2] < 3.0),np.where(a[:,2] > 0.2))  
+
+#     for i in circles:
+#         p=Polygon(polygon_list[i])
+#         if free_space.intersects(p):
+#             polygon.append(i)
+
+#     valid_polygon=a[polygon]
+
+#     print(len(valid_polygon),valid_polygon)
+#     # print(valid_polygon)
+
+    
+#     # for k in range(len(polygon_list)):
+#     #     p=Polygon(polygon_list[k])
+#     #     polygon.append(p)
 
     
     
-    # for i in range (len(polygon)):
-    #     if free_space.contains(polygon[i]) == True:
+#     # for i in range (len(polygon)):
+#     #     if free_space.contains(polygon[i]) == True:
 
-    #         xc,yc,r=fit_circle_2d(polygon_list[i][:,0],polygon_list[i][:,1])
-    #         if r >0.75 and r< 1.5:
-    #             circles.append(i)
+#     #         xc,yc,r=fit_circle_2d(polygon_list[i][:,0],polygon_list[i][:,1])
+#     #         if r >0.75 and r< 1.5:
+#     #             circles.append(i)
 
     
 
@@ -133,8 +227,25 @@ def angle_between(u, v, n=None):
 def pub():
     rospy.init_node('circle_fitting')
     rospy.Subscriber("/cloud_filtered_Box", senmsg.PointCloud2, point_cloud_callback)
-    rospy.Subscriber("/detection/lidar_detector/objects", autoware.DetectedObjectArray, callback_detectedobjects)
-    rospy.spin()
+    rospy.Subscriber("/visualization_MarkerArray", vismsg.MarkerArray, callback_markerarray)
+    pub_goal_midle=rospy.Publisher("/goal_midle",geomsg.PoseStamped,queue_size=1)
+
+
+    rate=rospy.Rate(20)
+    goal=geomsg.PoseStamped()
+    goal.header.frame_id="right_os1/os1_sensor"
+
+    while not rospy.is_shutdown():
+        if P is not None and slope is not None:
+            goal.pose.position.x=P[0]
+            goal.pose.position.y=P[1]
+            goal.pose.position.z= -1.36
+            goal.pose.orientation.z=np.sin(slope/2.0)
+            goal.pose.orientation.w=np.cos(slope/2.0)
+        
+        pub_goal_midle.publish(goal)
+    rate.sleep()
+    #rospy.spin()
 
 if __name__ == '__main__':
     try:
--- script/slalom.py
+++ script/slalom.py
@@ -9,10 +9,14 @@ import sensor_msgs.msg as senmsg
 from shapely.geometry import Polygon,Point
 import autoware_msgs.msg as autoware
 import obstacle_detector.msg as ob_det
+import geometry_msgs.msg as geomsg
+
 
 
 circles=None
 segments=None
+P=None
+slope=None
 
 
 def tysik_callback(data):
@@ -22,12 +26,33 @@ def tysik_callback(data):
 
     # for i in range(len(segments)):
     #     slope,intercept=slope_intercept(segments[i].first_point.x,segments[i].first_point.y,segments[i].last_point.x,segments[i].last_point.y)
-   
-    
 
+def callback_freespace_local(data):
+    global slope
+
+    points=data.points 
     
+    a=np.zeros((5,2))
+
+    polygon_data= np.zeros((len(points),2)) 
+    for i in range (len(points)):
+        polygon_data[i,0]=points[i].x
+        polygon_data[i,1]=points[i].y
+    #free_space=Polygon([[np.min(polygon_data[:,0]),np.max(polygon_data[:,1])],[np.max(polygon_data[:,0]),np.max(polygon_data[:,1])],[np.max(polygon_data[:,0]),np.min(polygon_data[:,1])],[np.min(polygon_data[:,0]),np.min(polygon_data[:,1])]])    
+
+    free_space=Polygon(polygon_data).minimum_rotated_rectangle
+
+
+    # a[:,0]=free_space.exterior.coords.xy[0]
+    # a[:,1]=free_space.exterior.coords.xy[1]
+    if free_space.exterior.coords.xy[0][0] < free_space.exterior.coords.xy[0][1]: 
+        slope=np.arctan2((free_space.exterior.coords.xy[1][0]-free_space.exterior.coords.xy[1][1]),(free_space.exterior.coords.xy[0][0]-free_space.exterior.coords.xy[0][1]))
+    elif free_space.exterior.coords.xy[0][0] > free_space.exterior.coords.xy[0][1]: 
+        slope=np.arctan2((free_space.exterior.coords.xy[1][1]-free_space.exterior.coords.xy[1][0]),(free_space.exterior.coords.xy[0][1]-free_space.exterior.coords.xy[0][0]))    
     
-def callback_freespace(data):
+def callback_freespace_global(data):
+    global P
+
     points=data.points 
     
     valid_circles=[]
@@ -41,7 +66,12 @@ def callback_freespace(data):
     #free_space=Polygon([[np.min(polygon_data[:,0]),np.max(polygon_data[:,1])],[np.max(polygon_data[:,0]),np.max(polygon_data[:,1])],[np.max(polygon_data[:,0]),np.min(polygon_data[:,1])],[np.min(polygon_data[:,0]),np.min(polygon_data[:,1])]])    
 
     free_space=Polygon(polygon_data).minimum_rotated_rectangle
-    
+    #print(free_space.exterior.coords.xy)
+
+    #slope_left,interception=slope_intercept(free_space.exterior.coords.xy[0][0],)
+
+    #slope=np.arctan2((free_space.exterior.coords.xy[1][1]-free_space.exterior.coords.xy[1][0]),(free_space.exterior.coords.xy[0][1]-free_space.exterior.coords.xy[0][0]))
+    #slope_right=np.arctan2((free_space.exterior.coords.xy[1][2]-free_space.exterior.coords.xy[1][3]),(free_space.exterior.coords.xy[0][2]-free_space.exterior.coords.xy[0][3]))
 
     if circles is not None:
         for i in range(len(circles)):
@@ -63,12 +93,19 @@ def callback_freespace(data):
     
     # #print(valid_circles[0].x
     if len(valid_circles)==2:
-        P=Point((valid_circles[0].x + valid_circles[1].x)/2,(valid_circles[0].y + valid_circles[1].y)/2)
+        P=((valid_circles[0].x + valid_circles[1].x)/2,(valid_circles[0].y + valid_circles[1].y)/2)
         #py=(valid_circles[0].y + valid_circles[1].y)/2
-        print(P)
+        #print(P)
+
+        
+    
+        # if pub_goal_midle is not None:
+        #     pub_goal_midle.publish(goal)
+
+
         #(np.max([valid_circles[0].y,valid_circles[1].y])) - np.abs(np.min([valid_circles[0].y,valid_circles[1].y]))-np.max([valid_circles[0].y,valid_circles[1].y])     
-    else:
-        print('nincs/tul sok kor')
+    # else:
+    #     print('nincs/tul sok kor',len(valid_circles))
 
 
         
@@ -76,14 +113,36 @@ def callback_freespace(data):
 def slope_intercept(x1,y1,x2,y2):
     slope = (y2 - y1) / (x2 - x1)
     interception = y1 - slope * x1     
-    return slope,interception
+    return np.arctan2(slope),interception
+
+
+
+
+
 
 def pub():
     rospy.init_node('slalom')
     rospy.Subscriber("/raw_obstacles", ob_det.Obstacles, tysik_callback)
-    rospy.Subscriber("/free_space_marker", vismsg.Marker, callback_freespace)
-    #rospy.Publisher()
-    rospy.spin()
+    rospy.Subscriber("/converted_bounding_poly", vismsg.Marker, callback_freespace_global)
+    rospy.Subscriber("/free_space_marker", vismsg.Marker, callback_freespace_local)
+
+    pub_goal_midle=rospy.Publisher("/goal_midle",geomsg.PoseStamped,queue_size=1)
+
+    rate=rospy.Rate(20)
+    goal=geomsg.PoseStamped()
+    goal.header.frame_id="map"
+
+    while not rospy.is_shutdown():
+        if P is not None and slope is not None:
+            goal.pose.position.x=P[0]
+            goal.pose.position.y=P[1]
+            goal.pose.position.z= -1.36
+            goal.pose.orientation.z=np.sin(slope/2.0)
+            goal.pose.orientation.w=np.cos(slope/2.0)
+        
+        pub_goal_midle.publish(goal)
+    rate.sleep()
+    #rospy.spin()
 
 if __name__ == '__main__':
     try:
