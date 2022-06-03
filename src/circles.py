f3a7e2648bbac677cbf7266e3318cbd756b20161
horverno <horverno@gmail.com>
2022-06-03 14:27:45 +0200

    fix transform error

--- src/circles.py
+++ src/circles.py
@@ -26,6 +26,8 @@ middle_pose = None
 pub_valid_circles = None
 pub_goal_midle = None
 pub_slope = None
+pub_slopel= None
+pub_sloper= None
 lenght_right = lenght_left = None
 local_slope=None
 EPS = None
@@ -37,6 +39,7 @@ min_distance = None
 trans_ouster_laser = None
 ouster_frame = None
 trans_ouster_ground_link = None
+xl0,xr0,yl0,yr0= None,None,None,None
 
 
 def tf_callback():
@@ -82,16 +85,15 @@ def point_cloud_callback(data):
     points[:,0]=pc['x']
     points[:,1]=pc['y']
     points[:,2]=pc['z']
-
-    
     
     xmask= np.logical_and(points[:,0] > min_x,points[:,0] < max_x)
     ymask= np.logical_and(points[:,1] > min_y, points[:,1] < max_y )
     zmask= points[:,2] > trans_ouster_ground_link.transform.translation.z + barrier_z
 
     mask=np.logical_and(np.logical_and(xmask,ymask),zmask) 
-    
 
+    #print(trans_ouster_laser)
+    
     points=points[mask]
     
 
@@ -99,7 +101,7 @@ def point_cloud_callback(data):
     if len(points) > 0:
         clustering=DBSCAN(eps=EPS,min_samples=Min_samples).fit(points[:,0:2])
 
-        number_clusters= len(set(clustering.labels_)) - (1 if -1 in clustering.labels_ else 0)
+        number_clusters= len(set(clustering.labels_)) #- (1 if -1 in clustering.labels_ else 0)
         b=np.zeros((number_clusters,3))
         points=np.column_stack((points,clustering.labels_))
         for i in range(number_clusters):
@@ -111,8 +113,8 @@ def point_cloud_callback(data):
             b[i,2]=r
         
 
-        circles_index = np.intersect1d(np.where(b[:,2] < max_radius),np.where(b[:,2] > min_radius))
-        valid_circles = b[circles_index]
+        
+        valid_circles = b
         area_compare()
 
     marker_arr=vismsg.MarkerArray() 
@@ -167,56 +169,111 @@ def fit_circle_2d(x, y, w=[]):
     return xc, yc, r
 
 def left_lane_callback(msg):
-    global trans_ouster_laser,slope_left,intercept_left,lenght_left
+    global trans_ouster_laser,slope_left,intercept_left,lenght_left,xl0,yl0,pub_slopel
     x=np.zeros(len(msg.points),)
     y=np.zeros(len(msg.points),)
     for i in range (len(msg.points)):
-        x[i]=msg.points[i].x + trans_ouster_laser.transform.translation.x
-        y[i]=msg.points[i].y + trans_ouster_laser.transform.translation.y
+        x[i]=msg.points[i].x #+ trans_ouster_laser.transform.translation.x
+        y[i]=msg.points[i].y #+ trans_ouster_laser.transform.translation.y
     lenght_left = math.sqrt((x[-1] - x[0])**2 + (y[-1] - y[0])**2)
     model = LinearRegression().fit(x.reshape(-1,1),y.reshape(-1,1))
     slope_left = model.coef_
     intercept_left = model.intercept_
-    #print(slope_left)
+
+    yl_u=(-slope_left*-30)+intercept_left
+    
+    mark_sl = vismsg.Marker()
+    mark_sl.header.stamp= rospy.Time.now()
+    mark_sl.header.frame_id = ouster_frame
+    mark_sl.type = mark_sl.LINE_STRIP
+    mark_sl.action = mark_sl.ADD
+    mark_sl.scale.x=mark_sl.scale.y=mark_sl.scale.z = 0.5
+    mark_sl.color.r = 0.3
+    mark_sl.color.g = 0.5
+    mark_sl.color.b = 0.9
+    mark_sl.color.a = 0.9 # 90% visibility
+    mark_sl.pose.orientation.x = mark_sl.pose.orientation.y = mark_sl.pose.orientation.z = 0.0
+    mark_sl.pose.orientation.w = 1.0
+    mark_sl.lifetime=rospy.Duration(0.1)
+    mark_sl.points=[]
+    if slope_left is not None:
+        p1l=geomsg.Point(); p1l.x=-x[-1];p1l.y=-y[-1]; p1l.z=0.0
+        p2l=geomsg.Point(); p2l.x=-30;p2l.y=-yl_u; p2l.z=0.0
+        mark_sl.points.append(p1l)
+        mark_sl.points.append(p2l)
+
+    pub_slopel.publish(mark_sl)
 
    
 def right_lane_callback(msg):
-    global trans_ouster_laser,slope_right,intercept_right,lenght_right
+    global trans_ouster_laser,slope_right,intercept_right,lenght_right,xr0,yr0,pub_sloper
     x=np.zeros(len(msg.points),)
     y=np.zeros(len(msg.points),)
     for i in range (len(msg.points)):
-        x[i]=msg.points[i].x + trans_ouster_laser.transform.translation.x
-        y[i]=msg.points[i].y + trans_ouster_laser.transform.translation.y
+        x[i]=msg.points[i].x #+ trans_ouster_laser.transform.translation.x
+        y[i]=msg.points[i].y #+ trans_ouster_laser.transform.translation.y
+    xr0=x[-1]
+    yr0=y[-1]    
     lenght_right = math.sqrt((x[-1] - x[0])**2 + (y[-1] - y[0])**2)
     model = LinearRegression().fit(x.reshape(-1,1),y.reshape(-1,1))
     slope_right = model.coef_
     intercept_right = model.intercept_
+
+    yr_u=(-slope_right*-30)+intercept_right
+
+    mark_sr = vismsg.Marker()
+    mark_sr.header.stamp= rospy.Time.now()
+    mark_sr.header.frame_id = ouster_frame
+    mark_sr.type = mark_sr.LINE_STRIP
+    mark_sr.action = mark_sr.ADD
+    mark_sr.scale.x=mark_sr.scale.y=mark_sr.scale.z = 0.5
+    mark_sr.color.r = 0.3
+    mark_sr.color.g = 0.5
+    mark_sr.color.b = 0.9
+    mark_sr.color.a = 0.9 # 90% visibility
+    mark_sr.pose.orientation.x = mark_sr.pose.orientation.y = mark_sr.pose.orientation.z = 0.0
+    mark_sr.pose.orientation.w = 1.0
+    mark_sr.lifetime=rospy.Duration(0.1)
+    mark_sr.points=[]
+    if slope_right is not None:
+        p1r=geomsg.Point(); p1r.x=-x[-1];p1r.y=-y[-1]; p1r.z=0.0
+        p2r=geomsg.Point(); p2r.x=-30;p2r.y=-yr_u; p2r.z=0.0
+        mark_sr.points.append(p1r)
+        mark_sr.points.append(p2r)
+
+    pub_sloper.publish(mark_sr)
+    
     
 
 def area_compare():
-    global valid_circles,intercept_right,intercept_left,slope_left,slope_right,middle_pose,slope, lenght_left,lenght_right,local_slope,max_distance,min_distance,ouster_frame
+    global valid_circles,intercept_right,intercept_left,slope_left,slope_right,middle_pose,slope, lenght_left,lenght_right,local_slope,max_distance,min_distance,ouster_frame,xr0,xl0,yr0,yl0,pub_sloper,pub_slopel,trans_ouster_laser
     
-
     if slope_left is not None and slope_right is not None:
         slope = (slope_left + slope_right)/2
         goal=geomsg.PoseStamped()
         goal.header.frame_id=ouster_frame
         
+        
         if len(valid_circles) > 0: #and slope_left is not None and slope_right is not None:
-            y_left = slope_left*valid_circles[:,0]+intercept_left
-            y_right = slope_right*valid_circles[:,0]+intercept_right
+            y_left = -(slope_left*(valid_circles[:,0]-trans_ouster_laser.transform.translation.x)+intercept_left+trans_ouster_laser.transform.translation.y)
+            y_right = -(slope_right*(valid_circles[:,0]-trans_ouster_laser.transform.translation.x)+intercept_right+trans_ouster_laser.transform.translation.y) 
+            #print(y_left,y_right,valid_circles[:,1])
+            #print(trans_ouster_laser.transform.translation.y)
             
-            y_mask = np.logical_and(valid_circles[:,1] > y_right,valid_circles[:,1] < y_left)
+            y_mask = np.logical_and(valid_circles[:,1] < y_right,valid_circles[:,1] > y_left)
+           
             
             valid_circles = valid_circles[y_mask[0]]
             positive_rotated_slope=slope[0]+(math.pi/2)
             negative_rotated_slope=slope[0]-(math.pi/2)
             
-            y=slope*25
+            y=slope*(25)
+            #yl=slope_left*25
+            #yr=slope_right*25
             
             mark_s = vismsg.Marker()
             mark_s.header.stamp= rospy.Time.now()
-            mark_s.header.frame_id = ouster_frame
+            mark_s.header.frame_id = 'base_link'
             mark_s.type = mark_s.LINE_STRIP
             mark_s.action = mark_s.ADD
             mark_s.scale.x=mark_s.scale.y=mark_s.scale.z = 0.5
@@ -233,8 +290,31 @@ def area_compare():
                 p2=geomsg.Point(); p2.x=25;p2.y=y; p2.z=0.0
                 mark_s.points.append(p1)
                 mark_s.points.append(p2)
-
             pub_slope.publish(mark_s)
+
+            # mark_sl = vismsg.Marker()
+            # mark_sl.header.stamp= rospy.Time.now()
+            # mark_sl.header.frame_id = ouster_frame
+            # mark_sl.type = mark_s.LINE_STRIP
+            # mark_sl.action = mark_s.ADD
+            # mark_sl.scale.x=mark_sl.scale.y=mark_sl.scale.z = 0.5
+            # mark_sl.color.r = 0.1
+            # mark_sl.color.g = 0.4
+            # mark_sl.color.b = 0.9
+            # mark_sl.color.a = 0.9 # 90% visibility
+            # mark_sl.pose.orientation.x = mark_sl.pose.orientation.y = mark_sl.pose.orientation.z = 0.0
+            # mark_sl.pose.orientation.w = 1.0
+            # mark_sl.lifetime=rospy.Duration(0.1)
+            # mark_sl.points=[]
+            # if yl is not None:
+            #     p1l=geomsg.Point(); p1l.x=xl0;p1l.y=yl0; p1l.z=0.0
+            #     p2l=geomsg.Point(); p2l.x=-25;p2l.y=yl; p2l.z=0.0
+            #     mark_sl.points.append(p1l)
+            #     mark_sl.points.append(p2l)
+
+            # pub_slopel.publish(mark_sl)
+            #pub_sloper.publish(mark_sr)
+
             a=[]
 
 
@@ -276,7 +356,7 @@ def area_compare():
     
 
 def pub():
-    global valid_circles,pub_valid_circles,pub_goal_midle,first_run,transform_received,pub_slope,topic_name
+    global valid_circles,pub_valid_circles,pub_goal_midle,first_run,transform_received,pub_slope,topic_name,pub_slopel,pub_sloper
     rospy.init_node('circle_fitting')
     srv=Server(SlalomConfig,config_callback)
     tf_callback()
@@ -287,6 +367,8 @@ def pub():
     pub_valid_circles= rospy.Publisher("/valid_circles",vismsg.MarkerArray,queue_size=1)
     pub_goal_midle=rospy.Publisher("/goal_middle",geomsg.PoseStamped,queue_size=1)
     pub_slope=rospy.Publisher("/slope",vismsg.Marker, queue_size=1)
+    pub_slopel=rospy.Publisher("/slope_left",vismsg.Marker, queue_size=1)
+    pub_sloper=rospy.Publisher("/slope_right",vismsg.Marker, queue_size=1)
 
     rospy.spin()
        
